936812f491355f1a57d5c9a3f169f4b0e13ee5e3
luckystars0612 <18520609@gm.uit.edu.vn>
2021-05-19 23:03:58 +0700

    first commit

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.login;
 import android.content.Intent;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,9 +12,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.login.MD5_C;
 import com.example.login.SQLiteConnector;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,20 +35,53 @@ public class MainActivity extends AppCompatActivity {
     private Button loginBtn;
     private Button registerBtn;
 
-    SQLiteConnector db = new SQLiteConnector(this);
+    //SQLiteConnector db = new SQLiteConnector(this);
 
-    private boolean authenticate(String username, String password) {
+    private boolean authenticate(String username, String password) throws UnsupportedEncodingException {
         TextView infoTxt = (TextView)findViewById(R.id.infoTxt);
+
+        String username_en= URLEncoder.encode(username,"UTF8");
+        MD5_C md5_= new MD5_C();
+        String hashpass=md5_.getMd5(password);
+        String password_en= URLEncoder.encode(hashpass,"UTF8");
 
         if (username.length() == 0 || password.length() == 0) {
             infoTxt.setText("Please fill in all fields!");
             return false;
         }
         try {
-            if (db.checkUser(username, password) == false) {
-                infoTxt.setText("Username or password was wrong!");
-                return false;
-            }
+            String home="http://10.0.154.190:8080/api/login.php";
+            String url=home+ "?user_name="+username_en+"&user_pass="+password_en;
+            Log.e("url",url);
+            // Request a string response from the provided URL.
+            RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
+            StringRequest stringRequest= new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject=new JSONObject(response);
+                                if(jsonObject.getInt("login_success")==1){
+                                    Intent welcome = new Intent(MainActivity.this, WelcomeActivity.class);
+                                    welcome.putExtra("username", username);
+                                    startActivity(welcome);
+                                }
+                                else{
+                                    infoTxt.setText("Username or password is wrong!!");
+                                }
+                            }catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("error register" , "Error");
+                        }
+                    }
+            );
+            queue.add(stringRequest);
         } catch (Exception e) {
             infoTxt.setText("An unexpected error! Try again");
             return false;
@@ -74,10 +120,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String username = usernameTxt.getText().toString();
                 String password = passwordTxt.getText().toString();
-                if (authenticate(username, password)) {
-                    Intent welcome = new Intent(MainActivity.this, WelcomeActivity.class);
-                    welcome.putExtra("username", username);
-                    startActivity(welcome);
+                try {
+                    if (authenticate(username, password)) {
+                        Log.e("login","successfull login!");
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
                 }
             }
         });
--- app/src/main/java/com/example/login/RegisterActivity.java
+++ app/src/main/java/com/example/login/RegisterActivity.java
@@ -2,43 +2,83 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    SQLiteConnector db = new SQLiteConnector(this);
-
-    private boolean register (String email, String username, String password) {
+    private boolean register(String email, String username, String password) throws UnsupportedEncodingException {
+        String email_en= URLEncoder.encode(email,"UTF8");
+        String username_en= URLEncoder.encode(username,"UTF8");
+        MD5_C md5_= new MD5_C();
+        String hashpass=md5_.getMd5(password);
+        String password_en= URLEncoder.encode(hashpass,"UTF8");
         TextView infoTxt = (TextView)findViewById(R.id.infoTxt2);
         if (email.length() == 0 || username.length() == 0 || password.length() == 0) {
             infoTxt.setText("Please fill in all fields!");
             return false;
         }
-
         try {
-            if (db.checkUser(email)) {
-                infoTxt.setText("Email has already registered!");
-                return false;
-            }
-            if (db.checkUser(username)) {
-                infoTxt.setText("Username has already registered!");
-                return false;
-            }
-            User user = new User();
-            user.setName(username);
-            user.setPassword(password);
-            user.setEmail(email);
-            db.addUser(user);
+            String home="http://10.0.154.190:8080/api/register.php";
+            String url=home+ "?user_name="+username_en+"&user_email="+email_en+"&user_pass="+password_en;
+            Log.e("url",url);
+            // Request a string response from the provided URL.
+            RequestQueue queue= Volley.newRequestQueue(RegisterActivity.this);
+            StringRequest stringRequest= new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject=new JSONObject(response);
+                                if(jsonObject.getInt("check_email")==1){
+                                    infoTxt.setText(" Email has already registered!");
+                                }
+                                if(jsonObject.getInt("check_name")==1){
+                                    infoTxt.setText(" Username has already registered!");
+                                }
+                                if(jsonObject.getInt("success")==1)
+                                {
+                                    Intent registerIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                    registerIntent.putExtra("username", username);
+                                    registerIntent.putExtra("password", password);
+                                    startActivity(registerIntent);
+                                }
+                            }catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("error register" , "Error");
+                        }
+                    }
+            );
+            queue.add(stringRequest);
+
         } catch(Exception e) {
             infoTxt.setText("An unexpected error! Try again");
             return false;
         }
-
         return true;
     }
     @Override
@@ -57,11 +97,12 @@ public class RegisterActivity extends AppCompatActivity {
                 String username = usernameTxt.getText().toString();
                 String password = passwordTxt.getText().toString();
                 String email = emailTxt.getText().toString();
-                if (register(email, username, password)) {
-                    Intent registerIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                    registerIntent.putExtra("username", username);
-                    registerIntent.putExtra("password", password);
-                    startActivity(registerIntent);
+                try {
+                    if (register(email, username, password)) {
+                        Log.e("register","register successfuly");
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
                 }
             }
         });
